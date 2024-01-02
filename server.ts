8c5fee9cd882c3387efe8c8dd7fb921e8f52c81e
Manan Jaiswal <z5481212@ad.unsw.edu.au>
2024-01-03 00:26:02 +1100

    restructured server.ts

--- server.ts
+++ server.ts
@@ -4,17 +4,19 @@ if (process.env.NODE_ENV !== 'production') {
 
 import express, { json, NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
+import { register, hashPassword } from './stage1';
+
 const app = express();
 const passport = require('passport');
 const flash = require('express-flash');
 const session = require('express-session');
 const methodOverride = require('method-override');
 
-const initialisePassport = require('./passport-config');
-initialisePassport(passport);
-
 app.use(json());
 app.use(express.urlencoded({ extended: false }));
+
+const initialisePassport = require('./passport-config');
+initialisePassport(passport);
 
 app.use(flash());
 app.use(session({
@@ -25,6 +27,7 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(methodOverride('_method'));
+
 // Middleware which allows us to see what requests are being recived
 app.use(morgan('dev'));
 
@@ -32,7 +35,6 @@ app.use(morgan('dev'));
 app.set("view engine", "ejs");
 
 
-import { register, hashPassword } from './stage1';
 app.get("/", checkAuthenticated, (req: Request, res: Response) => {
     console.log('Home Page');
     res.render("index.ejs");
@@ -42,17 +44,16 @@ app.get("/register", checkNotAuthenticated, (req: Request, res: Response) => {
     res.render("register.ejs");
 });
 
-app.get("/login", checkNotAuthenticated, (req: Request, res: Response) => {
-    res.render("login.ejs");
-});
-
-
 app.post("/register", checkNotAuthenticated, (req: Request, res: Response) => {
     const email = req.body.email;
     const password = hashPassword(req.body.password);
 
     const response = register(email, password);
     return res.redirect('/login');
+});
+
+app.get("/login", checkNotAuthenticated, (req: Request, res: Response) => {
+    res.render("login.ejs");
 });
 
 app.post("/login", checkNotAuthenticated, passport.authenticate('local', {
@@ -82,6 +83,7 @@ function checkNotAuthenticated(req: Request, res: Response, next: NextFunction) 
 
     next();
 }
+
 // This tells the server to listen for requests, it is listening on port 3000, and logs that when listening
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
